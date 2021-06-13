f82cd0df612bfc31e2f4ce2f468c512eea536548
Heavyrisem <pyo1748@naver.com>
2021-06-13 21:17:31 +0900

    buidl command fix

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
-import '../style/App.css';
+import '../style/Main.css';
 
-function App() {
+function Main() {
 	const [Display, setDisplay] = useState<string>();
 	const viewRef = useRef<HTMLVideoElement>(null);
 
@@ -9,7 +9,6 @@ function App() {
 
 	function StartRec() {
 		let constraints = { audio: false, video: { width: {ideal: 9999}, height: {ideal: 9999}, facingMode: "environment"} };
-		// alert()
 		if (!navigator.mediaDevices) return;
 		navigator.mediaDevices.getUserMedia(constraints).then(media => {
 			if (viewRef.current) {
@@ -19,30 +18,12 @@ function App() {
 				}
 			}
 		}).catch(e => {
-			console.log(e)
+			console.log(e);
+			alert("Err" + e);
 		})
 	}
 
 	async function Capture() {
-		// let q = document.querySelector('input');
-
-		// if (q && q.files) {
-		// 	let fd = new FormData();
-		// 	fd.append("file", q.files[0]);
-
-		// 	console.log(await q.files[0].arrayBuffer())
-
-
-		// 	// let OCR_Response = await fetch('https://localhost:3001/ocr', {
-		// 	// 	method: "POST",
-		// 	// 	// headers: {'Content-Type': 'form-data'},
-		// 	// 	headers: {},
-		// 	// 	body: fd
-		// 	// });
-		// 	// console.log(OCR_Response);
-		// }
-
-
 		if (viewRef.current) {
 			let cv = document.createElement('canvas');
 			cv.width = viewRef.current.videoWidth;
@@ -50,22 +31,16 @@ function App() {
 			cv.getContext('2d')?.drawImage(viewRef.current, 0, 0);
 
 			let data = atob(cv.toDataURL('image/png').split(',')[1]);
-			// var array = [];
-			// for (var i = 0; i < data.length; i++) {
-			// 	array.push(data.charCodeAt(i));
-			// }
 			let file = new Blob([new Uint8Array(Buffer.from(data, 'binary'))], {type: 'image/png'})
 			let fd = new FormData();
 			fd.append("file", file, "image.png");
-			console.log(Buffer.from(data, 'binary'), fd);
 
-			let OCR_Response = await fetch('https://192.168.1.71:3001/ocr', {
+			let OCR_Response = await fetch(`https://${process.env.ENDPOINT}/ocr`, {
 				method: "POST",
-				// headers: {'Content-Type': 'form-data'},
 				headers: {},
 				body: fd
 			});
-			// console.log(OCR_Response);
+
 			let OCR_Result: {result?: string, detail?: string} = await OCR_Response.json();
 			if (OCR_Result.result)
 				setDisplay(OCR_Result.result);
@@ -77,8 +52,6 @@ function App() {
 	return (
 		<div className="Comp">
 			<video ref={viewRef} playsInline></video>
-			{/* <input type="file" />
-			<button onClick={Capture}>123</button> */}
 
 			<div className="Bottom">
 
@@ -99,4 +72,4 @@ function App() {
 	);
 }
 
-export default App;
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import App from './component/App';
+import Main from './component/Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
